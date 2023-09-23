26f0b5c603b51c666553fe7c216ba7e0ae47b080
Robert Nitsch <mail@robertnitsch.de>
2023-09-23 09:59:07 +0200

    Refactor: Move StdoutVideoWriter to separate source files

--- src/main.cpp
+++ src/main.cpp
@@ -13,52 +13,11 @@
 #include <io.h>
 #endif
 
+#include "StdoutVideoWriter.h"
 #include "process_single_threaded.h"
 
 using namespace cv;
 using namespace std;
-
-class StdoutVideoWriter : public VideoWriter {
-  public:
-    StdoutVideoWriter() : VideoWriter() {
-#if 0
-        // For debugging
-        ofile.open("temp/StdoutVideoWriter.dat", std::ios::binary);
-#endif
-    }
-
-    void write(InputArray image) override {
-        cv::Mat frame = image.getMat();
-        assert(!frame.empty());
-        assert(frame.type() == CV_8UC3);
-        int count = frame.cols * frame.rows * 3;
-        auto written = fwrite(frame.data, 1, count, stdout);
-
-#if 0
-        // For debugging
-        ofile.write((const char *)frame.data, (size_t)frame.cols * frame.rows * 3);
-#endif
-
-        if (written != count) {
-            assert(false);
-            throw std::runtime_error("fwrite has not written all data");
-        }
-
-        totalWritten += written;
-    }
-
-    void release() override { fflush(stdout); }
-
-    bool isOpened() const override { return true; }
-
-    long getTotalWritten() const { return totalWritten; }
-
-  private:
-    long totalWritten = 0;
-#if 0
-    std::ofstream ofile; // For debugging
-#endif
-};
 
 // TODO: Add --col-range commandline parameter
 // TODO: Replace the positional framerate parameter with --framerate option
